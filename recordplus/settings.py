b460f258af4771d694e342946226f44884fc91f6
Nabil Affo <nabil.noble01@gmail.com>
2024-01-02 18:01:41 +0000

    edit requirements

--- recordplus/settings.py
+++ recordplus/settings.py
@@ -203,12 +203,13 @@ AUTH_PASSWORD_VALIDATORS = [
 
 CORS_ALLOW_ALL_ORIGINS = True
 
-# CORS_ALLOWED_ORIGINS = [
-#     "http://34.207.165.115/",
-#     "https://recordplus.onrender.com/",
-#     "http://127.0.0.1:8000/",
-#     "http://localhost:5173/"
-# ]
+CORS_ALLOWED_ORIGINS = [
+    "http://34.207.165.115/",
+    "https://recordplus.onrender.com/",
+    "http://127.0.0.1:8000/",
+    "http://localhost:5173/",
+    "recordplus.onrender.com"
+]
 
 # CORS_ALLOW_METHODS = [
 #     "GET",
